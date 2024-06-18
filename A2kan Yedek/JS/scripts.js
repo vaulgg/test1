524dcfff93ba3e58dcf97ed25fcf1d1e5f85e312
vaulgg <valouepng@gmail.com>
2024-06-18 16:49:47 +0300

    Add files via upload

---
+++ A2kan Yedek/JS/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Document is fully loaded and parsed');
+    // Diğer JavaScript işlevlerini burada tanımlayabilirsiniz.
+});
+
